fe302c5dc4ac57f7cbf9f3a47e9219701f5a55ce
Jay Sivakumaran <jsivakumaran@outlook.com>
2016-03-03 21:24:03 -0800

    still on chapter 2of brave clojure

--- src/clojure_noon/core.clj
+++ src/clojure_noon/core.clj
@@ -11,4 +11,41 @@
 (defn train [] (println "Choo Choo!"))
 
 (println "Cleanliness is the next to godliness")
-(map)
+
+;wrapping and slurping in emacs
+(+ 1 (* 2) 3 4)
+
+(if true
+  "By Zeus's hammer!"
+  "By Aquama's trident!")
+
+(if false
+  "By Zeus's hammer!"
+  "By Aquaman's trident!")
+
+(if false
+  "By Odin's Elbow")
+
+(if true
+  (do (println "Success!")
+      "By Zeus's hammer!")
+  (do (println "Failure!")
+      "By Aquaman's trident!"))
+
+(when true
+  (println "Success!")
+  "abra cadabra")
+
+(nil? 1)
+
+(nil? nil)
+
+(if "bears eats beets"
+  "bears beets Battlestar Galactica")
+
+(if nil
+  "This won't be the result because nil is false"
+  "nil is falsey")
+
+(= 1 1)
+
